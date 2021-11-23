b4623a2b9abf4eb3030dc3412b1fd7325a14acf5
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-11-23 13:24:47 +0000

    added tests of employee, engineer and intern

---
+++ team-profile-generator/src/lib/Employee.test.js
@@ -0,0 +1,13 @@
+describe("Employee", () => {
+  test("should be an instance of Employee", () => {});
+
+  test("should return the expected name", () => {});
+
+  test("should return the expected id", () => {});
+
+  test("should return the expected email", () => {});
+
+  test("should return the expected office number", () => {});
+
+  test("should return the expected role", () => {});
+});
