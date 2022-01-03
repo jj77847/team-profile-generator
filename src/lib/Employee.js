b6dcebe678199d79d66d59bfb47242fe6288e54c
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-01-03 22:12:50 +0000

    files and folders

--- src/lib/Employee.js
+++ src/lib/Employee.js
@@ -1,11 +1,24 @@
 class Employee {
-  constructor({ name, id, email }) {}
+  constructor({ name, id, email }) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
 
-  getName() {}
+  getName() {
+    return this.name;
+  }
 
-  getId() {}
+  getId() {
+    return this.id;
+  }
 
-  getEmail() {}
+  getEmail() {
+    return this.email;
+  }
 
-  getRole() {}
+  getRole() {
+    return "Employee";
+  }
 }
+module.exports = Employee;
