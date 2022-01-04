5b9b11829a14b85defd3f5056f5a919a3af09f3a
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-01-04 13:04:07 +0000

    updated tests

---
+++ src/lib/Employee.test.js
@@ -0,0 +1,11 @@
+describe("Employee", () => {
+  test("should be an instance of Employee", () => {});
+
+  test("should return the expected name", () => {});
+
+  test("should return the expected id", () => {});
+
+  test("should return the expected email", () => {});
+
+  test("should return the expected role", () => {});
+});
---
+++ src/lib/Engineer.test.js
@@ -0,0 +1,13 @@
+describe("Engineer", () => {
+  test("should be an instance of Engineer", () => {});
+
+  test("should return the expected name", () => {});
+
+  test("should return the expected id", () => {});
+
+  test("should return the expected email", () => {});
+
+  test("should return the expected github", () => {});
+
+  test("should return the expected role", () => {});
+});
